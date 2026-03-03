703803454d0d5e4315c67bfa61ac3abde7e9e4c7
ghandourak11-design <ghandourak11@gmail.com>
2026-03-03 14:28:46 -0500

    Updated index.js for discord.js v14 compatibility.

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-// Import necessary libraries and configure your bot
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, GatewayIntentBits } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
-// Command to get stock prices
-client.on('messageCreate', async message => {
-    if (message.content === '/stock') {
-        const embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Stock Prices')
-            .setDescription('Here are the latest stock prices:')
-            .addFields(
-                { name: 'AAPL', value: '$150', inline: true },
-                { name: 'GOOGL', value: '$2800', inline: true }
-            )
-            .setTimestamp();
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!embed') {
+        const embed = new EmbedBuilder()
+            .setTitle('Title')
+            .setDescription('Description');
         message.channel.send({ embeds: [embed] });
     }
 });
 
-// New command to get prices
-client.on('messageCreate', async message => {
-    if (message.content === '/prices') {
-        const embed = new MessageEmbed()
-            .setColor('#ff6633')
-            .setTitle('Prices Information')
-            .setDescription('Here are the latest prices of the products:')
-            .addFields(
-                { name: 'Product 1', value: '$20', inline: true },
-                { name: 'Product 2', value: '$35', inline: true }
-            )
-            .setFooter({ text: 'For more information, contact support.' })
-            .setTimestamp();
-        message.channel.send({ embeds: [embed] });
-    }
-});
-
-// Log in your bot using the token
-client.login('YOUR_BOT_TOKEN');
+client.login('YOUR_TOKEN');
